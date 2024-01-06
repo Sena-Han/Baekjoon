96977a4232342ba932cde1c89ba760c2d77456b3
SenaHan <hsysn1@naver.com>
2024-01-06 19:28:20 +0900

    2024-01-06

---
+++ 14730.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, C, K, sum = 0;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> C >> K;
+        sum += C * K;
+    }
+
+    cout << sum << '\n';
+
+    return 0;
+}
