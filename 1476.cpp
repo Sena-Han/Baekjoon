b8f9725b7e931536c2a17122e06cf90c17a88e97
SenaHan <hsysn1@naver.com>
2024-01-09 15:01:47 +0900

    2024-01-09

---
+++ 1476.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int E, S, M, year = 1;
+
+    cin >> E >> S >> M;
+
+    while (1)
+    {
+        if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0)
+            break;
+        else
+            year++;
+    }
+
+    cout << year << '\n';
+
+    return 0;
+}
