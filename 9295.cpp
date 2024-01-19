94dd3a68e789e85fe171f629d5957c943184fb9a
SenaHan <hsysn1@naver.com>
2024-01-19 21:54:06 +0900

    2024-01-19

---
+++ 9295.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T, a, b;
+
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> a >> b;
+        printf("Case %d: %d\n", i, a + b);
+    }
+
+    return 0;
+}
