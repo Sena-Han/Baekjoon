f06ab1a8a5b8c4a77765dd5d2b75cd8160d35fc3
SenaHan <hsysn1@naver.com>
2024-02-03 12:29:37 +0900

    2024-02-03

---
+++ 1037.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, A[50];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    sort(A, A + n);
+
+    cout << A[0] * A[n - 1] << '\n';
+
+    return 0;
+}
