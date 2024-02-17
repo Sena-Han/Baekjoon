7c1b9e09356017e6ce18b7148715b21aaec31685
SenaHan <hsysn1@naver.com>
2024-02-18 00:10:20 +0900

    2024-02-18

---
+++ 23802.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N * 5; i++)
+    {
+        if (i < N)
+        {
+            for (int j = 0; j < N * 5; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+        else
+        {
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+    }
+
+    return 0;
+}
