c187668cfd4067bfacb92d1db05f4f7c0284f7ed
SenaHan <hsysn1@naver.com>
2024-02-26 15:43:30 +0900

    2024-02-26

---
+++ 23812.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N * 5; i++)
+    {
+        if (i < N)
+        {
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            for (int j = 0; j < N * 3; j++)
+                cout << " ";
+
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+        else if (i < N * 2)
+        {
+            for (int j = 0; j < N * 5; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+        else if (i < N * 3)
+        {
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            for (int j = 0; j < N * 3; j++)
+                cout << " ";
+
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+        else if (i < N * 4)
+        {
+            for (int j = 0; j < N * 5; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+        else
+        {
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            for (int j = 0; j < N * 3; j++)
+                cout << " ";
+
+            for (int j = 0; j < N; j++)
+                cout << "@";
+
+            cout << '\n';
+        }
+    }
+
+    return 0;
+}
