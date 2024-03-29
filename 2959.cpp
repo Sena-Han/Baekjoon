7ba3368c1318b3095b6e11dae7260d9169083156
SenaHan <hsysn1@naver.com>
2024-03-29 17:07:36 +0900

    2024-03-29

---
+++ 2959.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a[4];
+
+	cin >> a[0] >> a[1] >> a[2] >> a[3];
+
+	sort(a, a + 4);
+
+	cout << a[0] * a[2] << '\n';
+
+	return 0;
+}
