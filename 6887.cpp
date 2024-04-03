6e162ac8fda888a841b7320b59e6289dedbd0a6f
SenaHan <hsysn1@naver.com>
2024-04-03 20:23:32 +0900

    2024-04-03

---
+++ 6887.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+    cout << "The largest square has side length " << (int)sqrt(N) << ".\n";
+
+    return 0;
+}
