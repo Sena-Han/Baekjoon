5988023289a2b9bd737ba6d57db3e569ec2f188c
sen <hsysn1@naver.com>
2024-07-15 12:35:02 +0900

    2024-07-15

---
+++ 15000 - 15999/15651.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 9
+
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+void dfs(int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+			cout << arr[i] << " ";
+
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		visited[i] = true;
+		arr[cnt] = i;
+
+		dfs(cnt + 1);
+
+		visited[i] = false;
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	dfs(0);
+
+	return 0;
+}
