f15e02ac5f15828ce2aec9dfb9767ac7b83a9e66
sen <hsysn1@naver.com>
2024-10-23 22:31:22 +0900

    2024-10-23

---
+++ 15000 - 15999/15921.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    if (n != 0)
+        cout << "1.00" << '\n';
+    else
+        cout << "divide by zero" << '\n';
+
+    return 0;
+}
