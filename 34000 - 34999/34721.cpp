a9c62623468da5640c2f3814848f31c69d09ac97
sen <hsysn1@naver.com>
2025-11-16 14:10:48 +0900

    2025-11-10

---
+++ 34000 - 34999/34721.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+
+    cin >> N;
+
+    while (N--)
+        cout << "I love DGU" << '\n';
+
+    return 0;
+}
