df265491413a60d229f95d7a6784018fd28cd10f
sen <hsysn1@naver.com>
2026-03-18 22:52:19 +0900

    2026-03-18

---
+++ 09000 - 09999/9372.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+
+	cin >> T;
+
+	while (T--)
+	{
+		int N, M, a, b;
+
+		cin >> N >> M;
+
+		while (M--)
+			cin >> a >> b;
+
+		cout << N - 1 << '\n';
+	}
+
+	return 0;
+}
